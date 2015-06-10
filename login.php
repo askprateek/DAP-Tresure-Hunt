2695b1e405bfe717fd4c4ba318f3ba4d2785472f
Digital Artist Prateek <askpk123@gmail.com>
2015-06-11 01:31:59 +0530

    Removed Logged in User code

--- login.php
+++ login.php
@@ -3,25 +3,19 @@
 ob_start();
 session_start();
 include("dbconnect.php");
-if (!isset($_SESSION["login_user"]))
+$user=mysql_real_escape_string(stripslashes($_POST["username"]));
+$pass=md5(mysql_real_escape_string(stripslashes($_POST["password"])));
+
+$sql="SELECT * FROM users WHERE username='$user' and password='$pass' ";
+$result=mysql_query($sql);
+$count=mysql_num_rows($result);
+if ($count==1)
 {
-	$user=mysql_real_escape_string(stripslashes($_POST["username"]));
-	$pass=mysql_real_escape_string(stripslashes($_POST["password"]));
-
-	$sql="SELECT * FROM users WHERE username='$user' and password='$pass' ";
-	$result=mysql_query($sql);
-	if ($result)
-	{
-		$_SESSION["login_user"]=$user;
-		header("location:/home");
-	}
-	else {
-		$msg="Wrong Username or Password";
-	}
-
+	$_SESSION["login_user"]=$user;
+	header("location:/home");
 }
 else {
-	header("location: /home");
+	echo "Wrong Username or Password";
 }
 ob_end_flush();
 ?>
