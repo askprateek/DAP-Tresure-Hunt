f8456c92d562d05c30a16cba741717155f0648f2
Digital Artist Prateek <askpk123@gmail.com>
2015-06-11 16:02:31 +0530

    Answer box

--- home/index.php
+++ home/index.php
@@ -4,55 +4,63 @@ require("../dbconnect.php");
 if( isset( $_SESSION["login_user"]))
 {
   $user=$_SESSION["login_user"];
-?>
+  ?>
 
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>DAP Treasure Hunt</title>
-  <link rel="stylesheet" type="text/css" href="../css/dap.css">
-  <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
-</head>
+  <html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>DAP Treasure Hunt</title>
+    <link rel="stylesheet" type="text/css" href="../css/dap.css">
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+  </head>
   <title> Welcome <?php $_SESSION["login_user"]; ?> </title>
   <body>
     <div class ="container">
       <div class="panel panel-default">
-      <div class ="panels-body">
-      <ul class = "nav nav-tabs">
-        <li class ="active"> <a href="/" >Home</a> </li>
-        <li> <a href="leaderboard.php">Leaderboard</a> </li>
-        <li> <a href="../../logout.php">Logout</a> </li>
-      </ul>
-      Welcome <?php  echo $_SESSION["login_user"]; ?>
-    </div>
+        <div class ="panels-body">
+          <ul class = "nav nav-tabs">
+            <li class ="active"> <a href="/" >Home</a> </li>
+            <li> <a href="leaderboard.php">Leaderboard</a> </li>
+            <li> <a href="../../logout.php">Logout</a> </li>
+          </ul>
+          Welcome <?php  echo $_SESSION["login_user"]; ?>
+        </div>
 
-  </div>
-  <div class = "row">
-    <div class="col-sm-12">
+      </div>
+      <div class = "row">
+        <div class="col-sm-12" align ="center">
 
-      <h1 align="center"> Question will be shown here </h1>
+          <h1 align="center"> Question will be shown here </h1>
+          <?php
+          //  $user=$_SESSION["login_user"];
+          //echo $user;
+          $sql= "SELECT level FROM users WHERE username = '$user'";
+          $level = mysql_fetch_row(mysql_query($sql));
+          $question = "SELECT * FROM questions WHERE q_id ='$level[0]'";
+          $result = mysql_query($question);
+          $result =mysql_fetch_row($result);
+          echo $result[1];
+
+          ?>
+
+          <form class="form-horizontal" role="form" action = "check.php">
+            <div class="form-group">
+              <label class="col-sm-2 control-label"></label>
+              <div class="col-sm-8">
+                <input class="form-control" id="focusedInput" type="text" placeholder = "Your Answer Here :v">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      </html>
+
       <?php
-    //  $user=$_SESSION["login_user"];
-      echo $user;
-      $sql= "SELECT level FROM users WHERE username = '$user'";
-      $level = mysql_fetch_row(mysql_query($sql));
-      $question = "SELECT * FROM questions WHERE q_id ='$level[0]'";
-      $result = mysql_query($question);
-      $result =mysql_fetch_row($result);
-      echo $result[1];
-
-      ?>
-    </div>
-  </div>
-  </div>
-
-</html>
-
-<?php
-}
-else {
-  echo "NO Access";
-}
-?>
+    }
+    else {
+      echo "NO Access";
+    }
+    ?>
