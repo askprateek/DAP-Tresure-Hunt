5a70b75c83df1e496c19ade2efd01ba90cd41479
Digital Artist Prateek <askpk123@gmail.com>
2015-06-12 01:57:48 +0530

    Correct answer temp file

---
+++ home/correct.php
@@ -0,0 +1,18 @@
+<?php
+?>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>DAP Treasure Hunt</title>
+  <link rel="stylesheet" type="text/css" href="../css/dap.css">
+  <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+</head>
+<body>
+<div class = "container">
+  <div class ="jumbotron">
+    ?>
+    <h1 align="center"> Correct Answer XD</h1>
+  </div>
+</div>
