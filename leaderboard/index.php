9664396bc5b2e00e4be70735338f8bf50173658e
Digital Artist Prateek <askpk123@gmail.com>
2015-06-19 01:15:43 +0530

    Query fix and UI design

--- leaderboard/index.php
+++ leaderboard/index.php
@@ -12,22 +12,47 @@ include("../dbconnect.php");
 
 </head>
 <body>
-<div class ="container" >
-  <div class ="jumbotron">
-    <?php
-    $sql = "SELECT * FROM users ORDER BY level DESC, time_stamp ASC";
-    $result =mysql_query($sql);
-    if (mysql_fetch_row($result) > 0)
-    {
-      while ($row=mysql_fetch_assoc($result))
-      {
-        echo $row['username']."  ".$row['email_id']."  ".$row['level'];?> <br><?php
-      }
-    }
+  <div class ="container" >
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#" class="brand-logo">DAP Treasure Hunt</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="../home">Home</a></li>
+          <li class = "active"><a href="/">Leaderboard</a></li>
+          <li><a href="../logout.php">logout</a></li>
+        </ul>
+      </div>
+    </nav>
+    <div class ="row">
+      <div class ="col-s10" align = "center">
+        <table>
+          <thead>
+            <tr>
+              <th data-field="id">Username</th>
+              <th data-field="name">Email ID</th>
+              <th data-field="price">Level</th>
+            </tr>
+          </thead>
+          <?php
+          $sql = "SELECT * FROM users ORDER BY level DESC, time_stamp ASC";
+          $result=mysql_query($sql);
+          while ($row=mysql_fetch_array($result))
+          {
+            ?>
+            <tbody>
+              <tr>
+                <td><?php echo $row['username']; ?></td>
+                <td><?php echo $row['email_id']; ?></td>
+                <td><?php echo $row['level']; ?></td>
+              </tr>
+              <?php
+            }
 
-    ?>
+            ?>
+          </div>
+        </div>
 
-  </div>
-</div>
-</body>
-</html>
+      </div>
+    </div>
+  </body>
+  </html>
