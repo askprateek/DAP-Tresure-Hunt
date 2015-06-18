ed2cb0283bfe271791b1cc75df0b768356d18dc5
Digital Artist Prateek <askpk123@gmail.com>
2015-06-18 23:53:45 +0530

    CSS Added

--- leaderboard/index.php
+++ leaderboard/index.php
@@ -1,6 +1,17 @@
 <?php
 include("../dbconnect.php");
 ?>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>DAP Treasure Hunt</title>
+  <link rel="stylesheet" type="text/css" href="../css/dap.css">
+  <link rel="stylesheet" type="text/css" href="../css/materialize.min.css">
+
+</head>
+<body>
 <div class ="container" >
   <div class ="jumbotron">
     <?php
@@ -15,5 +26,8 @@ include("../dbconnect.php");
     }
 
     ?>
+
   </div>
 </div>
+</body>
+</html>
