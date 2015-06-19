157ca05aa11f8b74bf61b3c3d0cb1e8f49d89c1a
Digital Artist Prateek <askpk123@gmail.com>
2015-06-19 23:59:28 +0530

    New Login form added

--- index.php
+++ index.php
@@ -9,47 +9,91 @@ else
 {
   ?>
   <html>
-      <head>
-        <!--Import materialize.css-->
-        <link type="text/css" rel="stylesheet" href="css/dap.css"  media="screen,projection"/>
-        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+  <head>
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/dap.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
 
-        <!--Let browser know website is optimized for mobile-->
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-      </head>
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+  </head>
   <body>
-    <div class ="container">
-      <div class="page-header">
-        <h1>Online Treasure Hunt</h1>
-      </div>
-      <div class ="login-box">
-        <fieldset><legend id ="legend">User Login </legend>
-          <form method = "POST" action = "login.php" name="User login">
-            <br><i class="mdi-action-account-box"></i> Username : <input id = "box" type ="text" name = "username" required ><br>
-            <br><i class="mdi-editor-border-color"></i> Password :<input id ="box" type = "password" name = "password" required> <br>
-            <br> <input id ="button" type ="submit" value ="Login" ><br>
-          </form>
-        </fieldset>
-      </div>
+    <div class="container" >
+    <div>
+      <h1>Online Treasure Hunt</h1>
+    </div>
+      <div class="row">
+        <div class="col s12 m6 l6">
+          <!-- Posts list is displayed here -->
+          <div class="widget-item z-depth-1">
 
-      <div class ="register">
-        <fieldset><legend>New User? Register Here</legend>
-          <form method ="POST" action="register.php" name="User register">
-            <br>Desired Username <input id ="box" type ="text" name="newuser" required><br>
-            <br>Your Email ID <input id ="box" type ="text" name ="email" required ><br>
-            <br>Password <input id ="box" type = "password" name ="newpass" required ><br>
-            <br><input id ="button" type ="submit" name ="user-register" value = "Register"><br>
-          </form>
-        </fieldset>
+              <div class="row">
+                <form class="col s12 offset" action ="login.php" method="POST">
+
+                    <div class="col s12">
+                      <b>Login Here</b>
+                    </div>
+                    <div class="input-field col s12">
+                      <i class="mdi-action-account-circle prefix"></i>
+                      <input id="username" type="text" class="validate">
+                      <label for="username">Username</label>
+                    </div>
+
+                    <div class="input-field col s12">
+                      <i class="mdi-action-https prefix"></i>
+                      <input id="password" type="password" class="validate">
+                      <label for="password">Password</label>
+                    </div>
+                    <div class="col s1">
+                    <button class="btn waves-effect waves-light" type="submit">Login<i class="mdi-action-lock-open right"></i></button>
+                  </div>
+
+                </form>
+
+              </div>
+            </div>
+          </div>
+
+      <div class="col s12 m6 l6">
+                <!-- widgets are displayed here -->
+                <div class="widget-item z-depth-1">
+
+                    <div class="row">
+                      <form class="col s12 offset">
+
+                          <div class="col s12">
+                            <b>Login Here</b>
+                          </div>
+                          <div class="input-field col s12">
+                            <i class="mdi-action-account-circle prefix"></i>
+                            <input id="username" type="text" class="validate">
+                            <label for="username">Username</label>
+                          </div>
+
+                          <div class="input-field col s12">
+                            <i class="mdi-action-https prefix"></i>
+                            <input id="password" type="password" class="validate">
+                            <label for="password">Password</label>
+                          </div>
+                          <div class="col s1">
+                          <button class="btn waves-effect waves-light" type="submit">Login<i class="mdi-action-lock-open right"></i></button>
+                        </div>
+
+                      </form>
+
+                    </div>
+                  </div>
+                </div>
       </div>
     </div>
 
-    <!--Import jQuery before materialize.js-->
-      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-      <script type="text/javascript" src="js/materialize.min.js"></script>
-    </body>
-  </html>
 
-  <?php
-}
-?>
+        <!--Import jQuery before materialize.js-->
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+      </body>
+      </html>
+
+      <?php
+    }
+    ?>
