42c5654a508ae01aeb1ee8a3de69291f64ebb1be
Digital Artist Prateek <askpk123@gmail.com>
2015-07-02 00:26:36 +0530

    logs atomatically adds to database

--- home/check.php
+++ home/check.php
@@ -3,10 +3,12 @@ session_start();
 require("../dbconnect.php");
 if (isset($_SESSION["login_user"]))
 {
-    $user_answer=$_POST["user_answer"];
-    $user=$_SESSION["login_user"];
-    $sql="SELECT level FROM users WHERE username = '$user'";
-    $level = mysql_fetch_row(mysql_query($sql));
+  $user_answer=$_POST["user_answer"];
+  $user=$_SESSION["login_user"];
+  $sql="SELECT level FROM users WHERE username = '$user'";
+  $level = mysql_fetch_row(mysql_query($sql));
+  $logs="INSERT INTO logs (name,qid,answer) VALUES ('$user','$level[0]','$user_answer')";
+  $indatabase=mysql_query($logs);
     $sql = "SELECT answer FROM questions WHERE q_id='$level[0]'";
     $ans=mysql_fetch_row(mysql_query($sql));
     if ($user_answer==$ans[0])
@@ -16,11 +18,11 @@ if (isset($_SESSION["login_user"]))
       $update=mysql_query($sql);
       if ($update)
       {
-        header("location:correct.php");
+        header("location:/hunt/correct.php");
       }
     }
     else {
-      header("location:/");
+      header("location:/hunt/home");
     }
 
 }
